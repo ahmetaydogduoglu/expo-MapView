e6c16d869f9fac9ac8c6ba8f61b49d9aedbde62f
Ahmet Aydoğduoğlu <47844516+ahmetaydogduoglu@users.noreply.github.com>
2020-02-13 00:46:19 +0300

    commit code

--- components/MapViewCom.js
+++ components/MapViewCom.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet ,Dimensions } from "react-native"
+import { View, Text, StyleSheet, Dimensions } from "react-native"
 import MapView from 'react-native-maps';
-import {Marker} from "react-native-maps"
+import { Marker } from "react-native-maps"
 export default class MapViewCom extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            region: null
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
                 <Text style={{ textAlign: "center", fontSize: 30, marginTop: 19 }}>Map</Text>
                 <MapView
-                style={styles.mapStyle}
-
+                    style={styles.mapStyle}
                     initialRegion={{
                         latitude: 40,
                         longitude: -122.4324,
@@ -29,5 +34,5 @@ const styles = StyleSheet.create({
     mapStyle: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
-      },
+    },
 })
